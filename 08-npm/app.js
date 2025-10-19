87ba800af0bc53758555500026a0a85b570bb72b
Farrel Putra <farrelputradev@gmail.com>
2025-10-19 23:17:40 +0700

    initialize npm project

---
+++ 08-npm/app.js
@@ -0,0 +1,14 @@
+console.log('Hello NPM');
+
+// to initialize the package.json file
+// npm init
+
+// to install a package
+// npm install <package-name>
+// npm i <package-name>
+// npm i validator
+// npm i validator@13.15.15
+
+// to uninstall a package
+// npm uninstall <package-name>
+// npm un <package-name>

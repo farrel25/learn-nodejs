47a30ef1a94032197f412d565bcdb8d289030d1f
Farrel Putra <farrelputradev@gmail.com>
2025-10-21 05:14:50 +0700

    using package from npm

--- 08-npm/app.js
+++ 08-npm/app.js
@@ -1,4 +1,3 @@
-console.log('Hello NPM');
 
 // to initialize the package.json file
 // npm init
@@ -12,3 +11,25 @@ console.log('Hello NPM');
 // to uninstall a package
 // npm uninstall <package-name>
 // npm un <package-name>
+
+// https://www.npmjs.com/package/nodemon
+
+
+const validator = require('validator');
+const chalk = require('chalk');
+
+console.log(validator.isEmail('farrel@gmail.com'));
+console.log(validator.isMobilePhone('082112341234', 'id-ID'));
+console.log(validator.isMobilePhone('072112341234', 'id-ID'));
+console.log(validator.isMobilePhone('82112341234', 'id-ID'));
+console.log(validator.isMobilePhone('08211234', 'id-ID'));
+console.log(validator.isNumeric('08211234'));
+console.log(validator.isNumeric('08211234a'));
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.bgBlue('Hello world!'));
+console.log(chalk.black.bgRed.bold.underline.italic('Hello world!'));
+
+const name = 'farrel';
+const message = chalk`lorem ipsum dolor {bgYellow.black sit amet}, consectetur {bgGreen.italic.black adipiscing elit}. my name is ${name}`;
+console.log(message);

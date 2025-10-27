d5c799de7ec36120956b02a8e2c1821b0133b323
Farrel Putra <farrelputradev@gmail.com>
2025-10-27 07:04:40 +0700

    add new contact from command line argument

--- contact-app/app.js
+++ contact-app/app.js
@@ -98,25 +98,110 @@
 //     });
 // };
 
+
+
+
+
+/*
+* CONTACT APP PART 1
+*/
+
+// const contact = require('./contact.js');
+// // const { createQuestion, saveContact } = require('./contact');
+//
+// const main = async () => {
+//     // const name = await questions1();
+//     // const email = await questions2();
+//     // const phoneNumber = await questions3();
+//
+//     // const name = await createQuestion('Enter your name: ');
+//     // const email = await createQuestion('Enter your email: ');
+//     // const phoneNumber = await createQuestion('Enter your phone number: ');
+//     //
+//     // saveContact(name, email, phoneNumber);
+//
+//     const name = await contact.createQuestion('Enter your name: ');
+//     const email = await contact.createQuestion('Enter your email: ');
+//     const phoneNumber = await contact.createQuestion('Enter your phone number: ');
+//
+//     contact.saveContact(name, email, phoneNumber);
+// }
+//
+// main();
+
+
+
+
+
+/*
+* CONTACT APP PART 2
+*/
+const yargs = require('yargs');
 const contact = require('./contact.js');
-// const { createQuestion, saveContact } = require('./contact');
 
-const main = async () => {
-    // const name = await questions1();
-    // const email = await questions2();
-    // const phoneNumber = await questions3();
+// Take arguments from the command line
+// console.log(process.argv);
+// node app athaillah
+// [
+//   '/usr/local/bin/node',
+//   '/Users/farrelputra/Workspace/wpu/learn-nodejs/contact-app/app',
+//   'athaillah'
+// ]
+//
+// console.log(process.argv[2]);
+// node app athaillah
+// athaillah
 
-    // const name = await createQuestion('Enter your name: ');
-    // const email = await createQuestion('Enter your email: ');
-    // const phoneNumber = await createQuestion('Enter your phone number: ');
-    //
-    // saveContact(name, email, phoneNumber);
 
-    const name = await contact.createQuestion('Enter your name: ');
-    const email = await contact.createQuestion('Enter your email: ');
-    const phoneNumber = await contact.createQuestion('Enter your phone number: ');
 
-    contact.saveContact(name, email, phoneNumber);
-}
+// console.log(yargs.argv);
+// node app add --fullName="Farrel"
+// { _: [ 'add' ], fullName: 'Farrel', '$0': 'app' }
+//
+// yargs.command(
+//     'add',
+//     'Add a new contact',
+//     () => {},
+//     (argv) => {
+//         console.log(argv.fullName);
+//     }
+// );
+//
+// yargs.parse();
 
-main();
+
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new contact',
+    builder: {
+        fullName: {
+            describe: 'Full name of the contact',
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: 'Email of the contact',
+            demandOption: false,
+            type: 'string'
+        },
+        phoneNumber: {
+            describe: 'Phone number of the contact',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        const contactRequest = {
+            name: argv.fullName,
+            email: argv.email,
+            phoneNumber: argv.phoneNumber
+        }
+        console.log(contactRequest);
+        contact.saveContact(contactRequest.name, contactRequest.email, contactRequest.phoneNumber);
+    }
+});
+
+yargs.parse();
+
+// node app add --fullName="Putra" --email="putra@domain.com" --phoneNumber="0822"
--- contact-app/contact.js
+++ contact-app/contact.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
-const readline = require('readline');
+const chalk = require('chalk');
+const validator = require('validator');
+// const readline = require('readline');
 
-const rl = readline
-    .createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+// const rl = readline
+//     .createInterface({
+//         input: process.stdin,
+//         output: process.stdout
+//     });
 
 // Create a data directory if it doesn't exist
 const dirPath = './data';
@@ -19,17 +21,32 @@ if (!fs.existsSync(contactFilePath)) {
     fs.writeFileSync(contactFilePath, '[]', 'utf8');
 }
 
-const createQuestion = (question) => {
-    return new Promise((resolve, reject) => {
-        rl.question(
-            question,
-            answer => resolve(answer)
-        );
-    });
-};
+// const createQuestion = (question) => {
+//     return new Promise((resolve, reject) => {
+//         rl.question(
+//             question,
+//             answer => resolve(answer)
+//         );
+//     });
+// };
 
 const saveContact = (name, email, phoneNumber) => {
     const contactRequest = {name, email, phoneNumber};
+
+    // Validate the email
+    if (contactRequest.email) {
+        const emailValid = validator.isEmail(contactRequest.email);
+        if (!emailValid) {
+            console.log(chalk.bgRed.white.bold('Email is not valid. Please enter a valid email.'));
+            return false;
+        }
+    }
+
+    const mobilePhoneValid = validator.isMobilePhone(contactRequest.phoneNumber, 'id-ID');
+    if (!mobilePhoneValid) {
+        console.log(chalk.bgRed.white.bold('Phone number is not valid. Please enter a valid phone number.'));
+        return false;
+    }
 
     let contacts;
     let existingContactsJsonString;
@@ -42,7 +59,18 @@ const saveContact = (name, email, phoneNumber) => {
 
     if (existingContactsJsonString) {
         contacts = JSON.parse(existingContactsJsonString);
+
+        // Check if the name already exists in the contacts array
+        const nameExists = contacts.find(contact => contact.name === contactRequest.name);
+
+        if (nameExists) {
+            console.log(chalk.bgRed.white.bold('Name already exists in the Contacts. Please enter a different name.'));
+            return false;
+        }
+
+        // Add the new contact to the contacts array
         contacts.push(contactRequest);
+
     } else {
         console.log('No existing contacts');
         contacts = [contactRequest];
@@ -54,9 +82,10 @@ const saveContact = (name, email, phoneNumber) => {
         throw error;
     }
 
-    console.log(`Thanks ${contactRequest.name}, your contact is saved to the JSON file`);
+    console.log(chalk`{bgGreen.black.bold Thanks ${contactRequest.name}, your contact is saved to the JSON file}`);
 
-    rl.close();
+    // rl.close();
 }
 
-module.exports = { createQuestion, saveContact };
+// module.exports = { createQuestion, saveContact };
+module.exports = { saveContact };
